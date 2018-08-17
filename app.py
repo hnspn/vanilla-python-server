7a2f32f2701242e2a4ce52d87bad2501468d60c9
hnspn <bobmatt911@gmail.com>
2018-08-17 08:36:43 -0400

    listen to requests on a specified port

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+from socket import socket, gethostname
+
+server = socket()
+
+server.bind(('0.0.0.0', 5000))
+server.listen()
+
+while True:
+    client_socket, address = server.accept()
+    print(f'New connection from {address}')
+    help(client_socket)
+    client_socket.send(b'Thanks')
+    client_socket.close()
